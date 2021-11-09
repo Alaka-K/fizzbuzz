e1e223801c73f10015e7ac5ea09f5f693b46e648
kehinde alaka <kennyalaka93@gmail.com>
2021-11-09 15:55:16 +0000

    defined our method and spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,15 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when divisible by 3' do
   expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" when divisible by 5' do
+  expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns the input number when passed another number that is not divisible by 3 or 5' do
+  expect(fizzbuzz(4)).to eq 4
+  end
+  it 'returns "fizzbuzz" when the number passed is divisible buy 3 and 5' do
+  expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
